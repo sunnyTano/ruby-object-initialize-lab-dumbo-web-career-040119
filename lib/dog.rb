b4bdd4766e5c6644a5bf668339e9fe3a2aabc9c2
Susan Tan <ssusantan9@gmail.com>
2019-04-03 06:32:57 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  
+  def initialize(dog_name)
+    @dog_name = dog_name
+  end
+  
+  def dog_name=(dog_name)
+    @dog_name = dog_name
+  end
+  
+  def dog_name
+    @dog_name
+  end
+  
+end 
