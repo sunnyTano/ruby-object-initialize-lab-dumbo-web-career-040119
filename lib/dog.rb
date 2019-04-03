be51a3466df4147e8f9d9085bb065c543d8150a0
Susan Tan <ssusantan9@gmail.com>
2019-04-03 06:37:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  
+  def initialize(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
+  end
+  
+  def dog_name=(dog_name)
+    @name = dog_name
+  end
+  
+  def dog_name
+    @name
+  end
+  
+end 
